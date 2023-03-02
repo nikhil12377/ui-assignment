2d8627bf24c0fcf45fac63ea800755e995183c46
Nikhil <nikhild12377@gmail.com>
2023-03-02 11:45:32 +0530

    Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+$("input:radio:checked").next("label").addClass("checked");
+$("input:checkbox:checked").next("label").addClass("checked");
+$(".selector_one").click(function () {
+  $(".list_one").toggleClass("hide");
+});
+$(".selector_two").click(function () {
+  $(".list_two").toggleClass("hide");
+});
+$(".selector_three").click(function () {
+  $(".list_three").toggleClass("hide");
+});
+$(".selector_four").click(function () {
+  $(".list_four").toggleClass("hide");
+});
+$(".options_one").click(function () {
+  $(".list_one").toggleClass("hide");
+});
+$(".options_two").click(function () {
+  $(".list_two").toggleClass("hide");
+});
+$(".options_three").click(function () {
+  $(".list_three").toggleClass("hide");
+});
+$(".options_four").click(function () {
+  $(".list_four").toggleClass("hide");
+});
